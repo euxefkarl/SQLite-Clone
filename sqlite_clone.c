a3cc91078161608754cce0320e36db12dac54207
euxefkarl <blancadaeu@gmail.com>
2025-04-21 00:13:30 +0800

    Debugged unrun code in main. Forgot to remove initial exit function handling.

--- sqlite_clone.c
+++ sqlite_clone.c
@@ -97,7 +97,7 @@ typedef enum{
 }PrepareResult;
 
 MetaCommandResult do_meta_command(InputBuffer* input_buffer){
-    if(strcmp(input_buffer->buffer,'.exit') == 0){
+    if(strcmp(input_buffer->buffer,".exit") == 0){
       exit(EXIT_SUCCESS);
     }else{
       return META_COMMAND_UNRECOGNIZED_COMMAND;
@@ -115,11 +115,11 @@ typedef struct {
 }Statement;
 
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){ 
-    if(strcmp(input_buffer->buffer,'insert',6)==0){
+    if(strncmp(input_buffer->buffer,"insert",6)==0){
       statement->type = STATEMENT_INSERT;
       return PREPARE_SUCCESS;
     }
-    if(strcmp(input_buffer->buffer,'select')==0){
+    if(strcmp(input_buffer->buffer,"select")==0){
       statement->type = STATEMENT_SELECT;
       return PREPARE_SUCCESS;
     }
@@ -137,13 +137,13 @@ void execute_statement(Statement* statement){
     }
 }
 int main(int argc, char* argv[]) {
+  
   InputBuffer* input_buffer = new_input_buffer();
   while (true) {
     print_prompt();
     read_input(input_buffer);
 
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-      if(input_buffer->buffer[0]=='.'){
+    if(input_buffer->buffer[0]=='.'){
         switch (do_meta_command(input_buffer)){
         case (META_COMMAND_SUCCESS):
           continue;
@@ -160,8 +160,7 @@ int main(int argc, char* argv[]) {
           printf("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
           continue;
       }
-    } 
-    execute_statement(&statement);
-    printf("Executed. \n");
-  }
+      execute_statement(&statement);
+      printf("Executed. \n");
+    }   
 }
